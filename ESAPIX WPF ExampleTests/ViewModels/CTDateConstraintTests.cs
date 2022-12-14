829f5dd87645f4e7b21ea09577f579bbf8f7c9f0
Ramesh Boggula <spidersaint@gmail.com>
2022-12-14 18:51:14 +0100

    CT date constraint added.

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void CTDateConstraintPassesCorrectly()
+        {
+            //Arrange
+            var ctDate = DateTime.Now.AddDays(-59);
+
+            //Act
+            var actual = new CTDateConstraint().ConstraintCTDate(ctDate).ResultType;
+
+            //Assert
+            var expected = ResultType.PASSED;
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+            [TestMethod()]
+            public void CTDateConstraintFailsCorrectly()
+            {
+                //Arrange
+                var ctDate = DateTime.Now.AddDays(-61);
+
+                //Act
+                var actual = new CTDateConstraint().ConstraintCTDate(ctDate).ResultType;
+
+                //Assert
+                var expected = ResultType.ACTION_LEVEL_3;
+                Assert.AreEqual(expected, actual);
+
+
+            }
+        
+    }
+
+
+
+
+}
